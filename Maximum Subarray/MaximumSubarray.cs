173a554cf1b6e4b442aede16b8d734c4c62861cd
Joao <joaolfelicio@gmail.com>
2020-10-26 18:10:40 +0100

    Add 'Maximum Subarray' Solution

---
+++ Maximum Subarray/MaximumSubarray.cs
@@ -0,0 +1,20 @@
+using System;
+
+int MaxSubArray(int[] nums) 
+{
+    int sum = 0;
+    int maxSum = nums[0];
+
+    for (int i = 0; i < nums.Length; i++) {
+        sum += nums[i];
+        
+        if (nums[i] > sum) {
+            sum = nums[i];
+        }
+        
+        if (sum > maxSum) { 
+            maxSum = sum; 
+        }
+    }
+    return maxSum;
+    }
