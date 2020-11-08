759582fa80eba414271783e3ef9f45f8d2eb14a1
Joao <joaolfelicio@gmail.com>
2020-11-08 18:54:42 +0100

    Add 'Subtract The Product And Sum Of Digits Of An Integer' Solution

---
+++ Subtract The Product And The Sum Of An Integer/SubtractProductAndSumOfInteger.cs
@@ -0,0 +1,17 @@
+using System;
+
+int SubtractProductAndSum(int n) {
+
+    int product = 1;
+    int sum = 0;
+
+    while (n != 0)
+    {
+        int digit = n % 10;
+        product *= digit;
+        sum += digit;
+        n = n / 10;
+    }
+
+    return product - sum;
+}
