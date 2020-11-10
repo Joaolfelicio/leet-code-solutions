2679be2364d6b61cf0e3b1f0f9eda5617969c4d8
Joao <joaolfelicio@gmail.com>
2020-11-10 13:29:07 +0100

    Add max depth tree v2 solution

--- Maximum Depth Of Binary Tree/MaxDepthOfBinaryTree - V2.cs
+++ Maximum Depth Of Binary Tree/MaxDepthOfBinaryTree - V2.cs
@@ -2,31 +2,11 @@ using System;
 
 int MaxDepth(TreeNode root) 
 {
-    return MaxDepth(root, 0);
-}
-
-int MaxDepth(TreeNode node, int level)
-{
     if(node == null) 
     {
         return 0;
     }        
     
-    level++;
-    
-    int max = level;        
-    
-    if(node.left != null)
-    { 
-        var temp = MaxDepth(node.left, level);
-        max = Math.Max(temp, max);
-    }
-    
-    if(node.right != null)
-    {
-        var temp = MaxDepth(node.right, level);
-        max = Math.Max(temp, max);
-    }
-    
-    return max;
+    return Math.Max(MaxDepth(node.left), MaxDepth(node.right)) + 1;
 }
+
