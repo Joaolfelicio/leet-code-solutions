1c9bec9c1c485569aa605892337f1059eeba7a07
Joao <joaolfelicio@gmail.com>
2020-11-10 07:41:03 +0100

    Add 'Find Numbers With Even Number Of Digits' Solution

---
+++ Find Numbers With Even Numbers Of Digits/FindNumsWithEvenNumsOfDigits.cs
@@ -0,0 +1,23 @@
+using System;
+
+public int FindNumbers(int[] nums) 
+{
+    return FindNumbers(nums, 0, 0);   
+}
+
+public int FindNumbers(int[] nums, int p1, int count)
+{
+    if(p1 >= nums.Length)
+    {
+        return count;
+    }
+    
+    int digitsCount = (int) Math.Floor(Math.Log10(nums[p1]) + 1);
+    
+    if(digitsCount % 2 == 0)
+    {
+        count++;
+    }
+       
+    return FindNumbers(nums, ++p1, count);
+}
