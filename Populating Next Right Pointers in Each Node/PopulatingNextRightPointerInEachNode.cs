ff00168d6bd58cb03824693e0fd5a53b727cc522
Joao <joaolfelicio@gmail.com>
2020-12-17 19:22:20 +0100

    Add 'Populating Next Right Pointers in Each Node' using recursion

--- Populating Next Right Pointers in Each Node/PopulatingNextRightPointerInEachNode.cs
+++ Populating Next Right Pointers in Each Node/PopulatingNextRightPointerInEachNode.cs
@@ -23,6 +23,7 @@ public class Node {
 }
 */
 
+// BFS Approach
 public class Solution 
 {
     public Node Connect(Node root) 
@@ -52,4 +53,31 @@ public class Solution
         }        
         return root;
     }   
+}
+
+// Recursive way
+public class Solution2 
+{
+    public Node Connect(Node root) 
+    {
+        connect(root);
+        return root;
+    }   
+    
+    private void connect(Node root) {
+        
+        if(root == null)
+            return;
+
+        if(root.left != null){
+
+            root.left.next = root.right;
+
+            if(root.next != null)
+                root.right.next = root.next.left;
+        }
+
+        connect(root.left);
+        connect(root.right);
+    }
 }
