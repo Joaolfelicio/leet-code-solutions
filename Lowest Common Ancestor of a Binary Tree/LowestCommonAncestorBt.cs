a30776b7936c6ea467e9e010bc10987ea10abb72
Joao <joaolfelicio@gmail.com>
2020-12-18 07:28:17 +0100

    Add LowestCommonAncestorBt BFS approach

--- Lowest Common Ancestor of a Binary Tree/LowestCommonAncestorBt.cs
+++ Lowest Common Ancestor of a Binary Tree/LowestCommonAncestorBt.cs
@@ -7,6 +7,8 @@
  *     public TreeNode(int x) { val = x; }
  * }
  */
+
+ // Brute force
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) 
     {       
@@ -48,5 +50,35 @@ public class Solution {
         
         populateHashAncestor(root.left, ancestorTable);
         populateHashAncestor(root.right, ancestorTable);
+    }
+}
+
+// Recursive BFS
+public class Solution2 {
+    
+    private TreeNode Ancestor = null;
+    
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) 
+    {       
+        LowestCommonAncestorHelper(root, q, p);
+        return Ancestor;
+    }
+    
+    private bool LowestCommonAncestorHelper(TreeNode currNode, TreeNode p, TreeNode q)
+    {
+        if(currNode == null) return false;
+        
+        int leftSide = LowestCommonAncestorHelper(currNode.left, q, p) ? 1 : 0;
+        
+        int rightSide = LowestCommonAncestorHelper(currNode.right, q, p) ? 1 : 0;
+        
+        int mid = (currNode == p || currNode == q) ? 1 : 0;
+        
+        if(mid + leftSide + rightSide >= 2)
+        {
+            Ancestor = currNode;
+        }
+        
+        return (mid + leftSide + rightSide > 0);
     }
 }
