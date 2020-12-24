17e628a912e9396c11106b970eb8cac36dd305bb
Joao <joaolfelicio@gmail.com>
2020-12-24 13:33:15 +0100

    Add Sum Of Two Integers without '+'

---
+++ Sum Of Two Integers/SumOfTwoIntegers.cs
@@ -0,0 +1,15 @@
+public class Solution 
+{
+    public int GetSum(int a, int b) 
+    {       
+        while(b != 0)
+        {
+            int carry = a & b;
+            
+            a = a ^ b;
+            
+            b = carry << 1;
+        }
+        return a;
+    }
+}
