023ec3548a96cbe64ddc884cb07c985e42fbdbe9
Joao <joaolfelicio@gmail.com>
2021-01-13 07:24:01 +0100

    Remove Result List since we don't need it

--- Top K Frequent Words/TopKFrequentWords.cs
+++ Top K Frequent Words/TopKFrequentWords.cs
@@ -1,9 +1,7 @@
 public class Solution 
 {
     public IList<string> TopKFrequent(string[] words, int k) 
-    {
-        var result = new List<string>(k);
-        
+    {      
         var frequency = new Dictionary<string, int>();
         
         foreach(var word in words)
