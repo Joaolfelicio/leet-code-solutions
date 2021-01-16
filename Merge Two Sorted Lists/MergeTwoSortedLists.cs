a0b2c4757c9873c2ac43899ca6720557b4439519
Joao <joaolfelicio@gmail.com>
2021-01-16 18:31:29 +0100

    Refactor Merge Two Sorted Lists

--- Merge Two Sorted Lists/MergeTwoSortedLists.cs
+++ Merge Two Sorted Lists/MergeTwoSortedLists.cs
@@ -14,23 +14,34 @@ using System;
 
 ListNode MergeTwoLists(ListNode l1, ListNode l2) 
 {
-    ListNode head = new ListNode();
-    
-    ListNode current = head;
-    
-    while(l1 != null || l2 != null) 
-    {         
-        if(l1 != null && (l2 == null || l1.val <= l2.val)) 
+    if(l1 == null && l2 == null) return null;
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        
+        var dummyHead = new ListNode();
+        var currentDummyHead = dummyHead;
+        
+        var currentl1 = l1;
+        var currentl2 = l2;
+        
+        while(currentl1 != null && currentl2 != null)
         {
-            current.next = l1;
-            l1 = l1.next;
+            if(currentl1.val <= currentl2.val)
+            {
+                currentDummyHead.next = currentl1;
+                currentl1 = currentl1.next;
+            }
+            else
+            {
+                currentDummyHead.next = currentl2;
+                currentl2 = currentl2.next;  
+            }
+            
+            currentDummyHead = currentDummyHead.next;
         }
-        else
-        {
-            current.next = l2;
-            l2 = l2.next;
-        }
-        current = current.next;
-    }
-    return head.next;
+        
+        if(currentl2 != null) currentDummyHead.next = currentl2;
+        else if(currentl1 != null) currentDummyHead.next = currentl1;
+        
+        return dummyHead.next;
 }
