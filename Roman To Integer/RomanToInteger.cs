7fd7dbd5fdf61c6ad7cf81256f42fd63ee7abe16
Joao <joaolfelicio@gmail.com>
2021-01-27 19:14:20 +0100

    Refactor Roman To Integer

--- Roman To Integer/RomanToInteger.cs
+++ Roman To Integer/RomanToInteger.cs
@@ -1,25 +1,29 @@
-using System;
-
-int RomanToInt(string s) {
-    var romanValues = new Hashtable()
-    {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000},
-    };
-    
-    var sum = 0;
-    for(int i = 0; i < s.Length; i++) {
-        if(i == s.Length - 1 || (int) romanValues[s[i]] >= (int) romanValues[s[i + 1]]) {
-            sum += (int) romanValues[s[i]];
-        } else {
-            sum -= (int) romanValues[s[i]];
+public class Solution {
+    public int RomanToInt(string s) {
+        if(string.IsNullOrWhiteSpace(s)) return 0;
+        
+        var values = new Dictionary<char, int>()
+        {
+            {'M', 1000},
+            {'D', 500},
+            {'C', 100},
+            {'L', 50},
+            {'X', 10},
+            {'V', 5},
+            {'I', 1}
+        };
+        
+        var sum = 0;
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            
+            // If current char is smaller than next, we need to subtract   
+            if(i + 1 < s.Length && values[c] < values[s[i + 1]]) sum -= values[c];
+            else sum += values[c];
+            
         }
+        return sum;
     }
-    
-    return sum;
 }
