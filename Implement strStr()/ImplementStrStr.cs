82796b1a2b2faf56f0b68dd1a4f1f7af1e59f9f3
Joao <joaolfelicio@gmail.com>
2021-01-29 17:59:36 +0100

    Refactor ImplementStrStr

--- Implement strStr()/ImplementStrStr.cs
+++ Implement strStr()/ImplementStrStr.cs
@@ -1,41 +1,35 @@
-using System;
-
-int StrStr(string haystack, string needle) 
-{
-    if(String.IsNullOrEmpty(needle))
+public class Solution {
+    public int StrStr(string haystack, string needle) 
     {
-        return 0;
-    }
-    
-    int x = 0;
-    int pos = -1;
-    
-    for(int i = 0; i < haystack.Length; i++)
-    {                   
-        if(haystack[i] == needle[x])
-        {                               
-            x++;
+        if(String.IsNullOrEmpty(needle)) return 0;
+        if(needle.Length > haystack.Length) return -1;
+        
+        var hsPointer = 0;
+        var needlePointer = 0;
+        var startingIndex = -1;
+        
+        while(hsPointer < haystack.Length && needlePointer < needle.Length)
+        {
+            // If it's equal, incrase needle pointer
+            if(haystack[hsPointer] == needle[needlePointer])
+            {
+                // If it's the first char equal, startingIndex will be -1, so we should update the starting index
+                if(startingIndex == -1) startingIndex = hsPointer;
+                needlePointer++;
+            }
+            // If the chars are not equal and we already have a starting index, restart it and push back the haystack
+            // pointer to the starting index + 1
+            else if(startingIndex != -1)
+            {
+                needlePointer = 0;
+                hsPointer = startingIndex;
+                startingIndex = -1;
+            }
             
-            if(pos == -1)
-            {
-                pos = i;
-            }
+            hsPointer++;
         }
-        else
-        {
-            if(pos != -1)
-            {
-                i -= x;
-                pos = -1;
-                x = 0;
-            }
-        }
-                    
-        if(x == needle.Length)
-        {
-            return pos;
-        }
+        
+        // If the needle pointer is equal to the needle length, it means the needle has found
+        return needlePointer == needle.Length ? startingIndex : -1;
     }
-    
-    return -1;
 }
