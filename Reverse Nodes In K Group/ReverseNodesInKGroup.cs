4bf63325f9461be81764285e416f3f1cdcf39552
Joao <joaolfelicio@gmail.com>
2021-02-01 19:52:31 +0100

    Pushed wrong solution

--- Reverse Nodes In K Group/ReverseNodesInKGroup.cs
+++ Reverse Nodes In K Group/ReverseNodesInKGroup.cs
@@ -1,31 +1,52 @@
-using System;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
  *     public int val;
  *     public ListNode next;
- *     public ListNode(int val = 0, ListNode next = null) {
+ *     public ListNode(int val=0, ListNode next=null) {
  *         this.val = val;
  *         this.next = next;
  *     }
  * }
  */
-
-public class Solution {
-
-    public ListNode ReverseList(ListNode head) 
-    {       
-        ListNode prev = null;
-               
-        while(head != null)
+public class Solution 
+{
+    public ListNode ReverseKGroup(ListNode head, int k) 
+    {
+        var count = 0;
+        var ptr = head;
+        
+        while(ptr != null && count < k)
         {
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
+            ptr = ptr.next;
+            count++;
         }
         
-        return prev;
+        if(count != k) return head;
+        
+        var reversedHead = Reverse(head, k);
+        
+        head.next = ReverseKGroup(ptr, k);
+        return reversedHead;
+    }
+    
+    private ListNode Reverse(ListNode head, int k)
+    {
+        ListNode newHead = null;
+        var ptr = head;
+        
+        while(k > 0)
+        {
+            var nextNode = ptr.next;
+            
+            ptr.next = newHead;
+            newHead = ptr;
+            
+            ptr = nextNode;
+            
+            k--;
+        }
+        
+        return newHead;
     }
 }
