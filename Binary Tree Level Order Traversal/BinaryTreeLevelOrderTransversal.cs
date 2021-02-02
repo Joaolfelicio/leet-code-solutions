dcd7818864968b23f592b881db27a5a981bb5496
Joao <joaolfelicio@gmail.com>
2021-02-02 20:08:29 +0100

    Refactor Binary Tree Level Order Transversal

--- Binary Tree Level Order Traversal/BinaryTreeLevelOrderTransversal.cs
+++ Binary Tree Level Order Traversal/BinaryTreeLevelOrderTransversal.cs
@@ -1,35 +1,43 @@
-using System;
-
-IList<IList<int>> LevelOrder(TreeNode root) 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
 {
-    var result = new List<IList<int>>();
-    var queue = new Queue<TreeNode>();
-    
-    if(root == null) return result;
-    
-    queue.Enqueue(root);
-    
-    while(queue.Count > 0)
+    public IList<IList<int>> LevelOrder(TreeNode root) 
     {
-        var levelResult = new List<int>();
-        var levelSize = queue.Count;
+        var result = new List<IList<int>>();
         
-        for(var i = 0; i < levelSize; i++)
+        if(root == null) return result;
+        
+        var nodes = new Queue<TreeNode>();
+        nodes.Enqueue(root);
+        
+        while(nodes.Count > 0)
         {
-            var curr = queue.Dequeue();
+            var size = nodes.Count;
+            var subList = new List<int>();
             
-            if(curr.left != null)
+            for(int i = 0; i < size; i++)
             {
-                queue.Enqueue(curr.left);
+                var current = nodes.Dequeue();
+                
+                subList.Add(current.val);
+                
+                if(current.left != null) nodes.Enqueue(current.left);
+                if(current.right != null) nodes.Enqueue(current.right);
             }
-            if(curr.right != null)
-            {
-                queue.Enqueue(curr.right);
-            }
-            
-            levelResult.Add(curr.val);
-        }        
-        result.Add(levelResult);
-    }    
-    return result;
+            result.Add(subList);
+        }
+        return result;
+    }
 }
