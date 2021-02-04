9a490cc3b006da65bc5bf4cf9df53b9bec85da5b
Joao <joaolfelicio@gmail.com>
2021-02-04 19:33:14 +0100

    Add Course Schedule

---
+++ Course Schedule/CourseSchedule.cs
@@ -0,0 +1,56 @@
+public class Solution 
+{
+    public bool CanFinish(int numCourses, int[][] prerequisites) 
+    {
+        // The course and its list of course dependencies
+        var courseDict = BuildCourseGraph(prerequisites);
+        
+        var completed = new bool[numCourses];
+        var path = new bool[numCourses];
+
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(IsCyclic(i, courseDict, completed, path)) 
+                return false;
+        }
+        return true;
+    }
+    
+    private bool IsCyclic(int currCourse, Dictionary<int, List<int>> courseDict, bool[] completed, bool[] path)
+    {
+        // If we already completed the course or it has no dependencies, it cant be a cyclic
+        if(!courseDict.ContainsKey(currCourse) || completed[currCourse]) return false;
+
+        // If in the current path we already found this course, it's a cycle
+        if(path[currCourse]) return true;
+        
+        path[currCourse] = true;
+        
+        foreach(var dep in courseDict[currCourse])
+        {
+            if(IsCyclic(dep, courseDict, completed, path)) return true;
+        }
+        
+        path[currCourse] = false;
+        completed[currCourse] = true;
+        
+        return false;
+    }
+
+    
+    private Dictionary<int, List<int>> BuildCourseGraph(int[][] prerequisites)
+    {
+        var courseDict = new Dictionary<int, List<int>>();
+        
+        // Build the graph
+        foreach(var relation in prerequisites)
+        {
+            if(courseDict.ContainsKey(relation[1])) 
+                courseDict[relation[1]].Add(relation[0]);
+            else
+                courseDict.Add(relation[1], new List<int>() {relation[0]});
+        }
+        
+        return courseDict;
+    }
+}
