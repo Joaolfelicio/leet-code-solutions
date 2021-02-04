455f341da03c72aa974847b8a2dadaed4a8f36dd
Joao <joaolfelicio@gmail.com>
2021-02-04 17:23:51 +0100

    Refactor Number Of Islands

--- Number Of Islands/NumberOfIslands.cs
+++ Number Of Islands/NumberOfIslands.cs
@@ -2,38 +2,35 @@ public class Solution
 {
     public int NumIslands(char[][] grid) 
     {
-        var result = 0;
+        if(grid == null || grid.Length == 0) return 0;
         
-        if(grid == null || grid.Length == 0 || grid[0].Length == 0) return result;
+        var islands = 0;
         
         for(int i = 0; i < grid.Length; i++)
         {
-            for(int k = 0; k < grid[i].Length; k++)
+            for(int j = 0; j < grid[i].Length; j++)
             {
-                if(grid[i][k] == '1')
+                if(grid[i][j] == '1') 
                 {
-                    result++;
-                    TransverseIsland(i, k, grid);
+                    TransverseIslands(grid, i, j);
+                    islands++;
                 }
             }
         }
-        return result;
+        
+        return islands;
     }
     
-    private void TransverseIsland(int row, int column, char[][] grid)
+    private void TransverseIslands(char[][] grid, int row, int col)
     {
-        if(row >= grid.Length || row < 0 || 
-           column >= grid[row].Length || column < 0 || 
-           grid[row][column] == '0')
-        {
+        if(row < 0 || row >= grid.Length || col < 0 || col >= grid[row].Length || grid[row][col] == '0')
             return;
-        }
         
-        grid[row][column] = '0';
+        grid[row][col] = '0';
         
-        TransverseIsland(row - 1, column, grid);
-        TransverseIsland(row + 1, column, grid);
-        TransverseIsland(row, column - 1, grid);
-        TransverseIsland(row, column + 1, grid);
+        TransverseIslands(grid, row + 1, col);
+        TransverseIslands(grid, row - 1, col);
+        TransverseIslands(grid, row, col + 1);
+        TransverseIslands(grid, row, col - 1);
     }
 }
