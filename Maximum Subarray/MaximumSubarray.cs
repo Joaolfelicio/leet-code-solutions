8fd9e90178b6aa5bf2a29c1aaa776cac0c2c26d9
Joao <joaolfelicio@gmail.com>
2021-02-07 17:55:25 +0100

    Refactor Maximum Subarray

--- Maximum Subarray/MaximumSubarray.cs
+++ Maximum Subarray/MaximumSubarray.cs
@@ -1,20 +1,21 @@
-using System;
-
-int MaxSubArray(int[] nums) 
+public class Solution 
 {
-    int sum = 0;
-    int maxSum = nums[0];
+    public int MaxSubArray(int[] nums) 
+    {
+        if(nums == null || nums.Length == 0) return -1;
+        
+        int sum = 0;
+        int maxSum = nums[0];
 
-    for (int i = 0; i < nums.Length; i++) {
-        sum += nums[i];
-        
-        if (nums[i] > sum) {
-            sum = nums[i];
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            sum += nums[i];
+
+            // If the current num is bigger than the prev sum, we can overwrite
+            if (nums[i] > sum) sum = nums[i];
+            maxSum = Math.Max(maxSum, sum);
         }
-        
-        if (sum > maxSum) { 
-            maxSum = sum; 
-        }
+    
+        return maxSum;
     }
-    return maxSum;
-    }
+}
