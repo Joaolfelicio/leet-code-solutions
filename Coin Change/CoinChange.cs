9e0349faa06dbe76906023fa80a6d22685d9bb45
Joao <joaolfelicio@gmail.com>
2021-02-07 19:18:02 +0100

    Refactor Coin Change

--- Coin Change/CoinChange.cs
+++ Coin Change/CoinChange.cs
@@ -2,23 +2,26 @@ public class Solution
 {
     public int CoinChange(int[] coins, int amount) 
     {
-        int maxValue = amount + 1;
+        if(coins == null || coins.Length == 0 || amount == 0) return 0;
         
-        var tabulation = new int[maxValue];
-        Array.Fill(tabulation, maxValue);
+        var table = new int[amount + 1];
         
-        tabulation[0] = 0;
+        Array.Fill(table, -1);
         
-        for(int i = 1; i < tabulation.Length; i++) 
-        {           
-            foreach(int coin in coins)
-            {               
-                if(i >= coin)
-                {
-                    tabulation[i] = Math.Min(tabulation[i], tabulation[i - coin] + 1);
-                }
+        table[0] = 0;
+        
+        for(int i = 0; i < table.Length; i++)
+        {
+            if(table[i] == -1) continue;
+            
+            foreach(var coin in coins)
+            {
+                if(i + coin >= table.Length || coin > amount) continue;
+
+                if(table[i + coin] == -1) table[i + coin] = table[i] + 1;
+                else table[i + coin] = Math.Min(table[i] + 1, table[i + coin]);       
             }
         }
-        return tabulation[tabulation.Length - 1] != maxValue ? tabulation[tabulation.Length - 1] : -1;
+        return table[table.Length - 1];
     }
 }
