38e98295538cd1d05857f080df5cb7a99376b4b8
Joao <joaolfelicio@gmail.com>
2021-02-07 11:44:16 +0100

    Refactor Merge Intervals

--- Merge Intervals/MergeIntervals.cs
+++ Merge Intervals/MergeIntervals.cs
@@ -2,29 +2,23 @@ public class Solution
 {
     public int[][] Merge(int[][] intervals) 
     {
-        if(intervals == null || intervals.Length == 0) return new int[0][];
-     
+        if(intervals == null || intervals.Length == 0 || intervals[0].Length == 0)
+            return new int[0][];
+        
         Array.Sort(intervals, (a, b) => a[0] - b[0]);
         
-        var resultList = new List<int[]>();
-        resultList.Add(intervals[0]);
+        var result = new List<int[]>();
+        result.Add(intervals[0]);
         
         for(int i = 1; i < intervals.Length; i++)
-        {           
-            var lastMergeInterval = resultList.Count - 1;
+        {
+            var lastInterval = result[result.Count - 1];
+            var currInterval = intervals[i];
             
-            if(ShouldMerge(resultList[lastMergeInterval], intervals[i]))
-            {
-                resultList[lastMergeInterval][1] = Math.Max(intervals[i][1], resultList[lastMergeInterval][1]);
-            }
-            else
-            {
-                resultList.Add(intervals[i]);
-            }
+            if(lastInterval[1] >= currInterval[0]) 
+                lastInterval[1] = Math.Max(lastInterval[1], currInterval[1]);
+            else result.Add(currInterval);
         }
-               
-        return resultList.ToArray();
+        return result.ToArray();
     }
-    
-    private bool ShouldMerge(int[] interval1, int[] interval2) => interval1[1] >= interval2[0];
 }
