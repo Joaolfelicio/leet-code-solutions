b400eae63e7da88471a27e7c0f9f87caf11cb07b
Joao <joaolfelicio@gmail.com>
2021-02-12 20:53:22 +0100

    Refactor Analyze User Website Visit Pattern

--- Analyze User Website Visit Pattern/AnalyzeUserWebsiteVisitPattern.cs
+++ Analyze User Website Visit Pattern/AnalyzeUserWebsiteVisitPattern.cs
@@ -1,34 +1,24 @@
 public class Solution 
 {
     public IList<string> MostVisitedPattern(string[] username, int[] timestamp, string[] website) 
-    {  
-        var map = new Dictionary<string, List<Pair>>();
-        int n = username.Length;
+    {          
+        var userVisits = GetVisitInfos(username, timestamp, website);
         
-        // Collect the website info for every user, key: username, value : timestamp and website
-        for(int i = 0; i < n; i++)
-        {
-            if(!map.ContainsKey(username[i])) map.Add(username[i], new List<Pair>());
-            map[username[i]].Add(new Pair(timestamp[i], website[i]));
-        }
-        
-        
-        // Count map to record every 3 combinations occuring time for the different user
+        // Count userVisits to record every 3 combinations occuring time for the different user
         var count = new Dictionary<string, int>();
         
         string result = string.Empty;
         
-        foreach(var key in map.Keys)
+        foreach(var userVisit in userVisits.Keys)
         {
             // Set to avoid visit the same 3-seq in one user
             var set = new HashSet<string>();
             
-            var list = map[key];
+            var list = userVisits[userVisit];
             
             list.Sort((a, b) => a.Time - b.Time); // Sort by time
                 
             // Brute force O(N^3)
-            
             for(int i = 0; i < list.Count; i++)
             {
                 for(int j = i + 1; j < list.Count; j++)
@@ -52,32 +42,40 @@ public class Solution
                             
                             set.Add(str);
                         }
-                        if(result.Equals("") || count[result] < count[str] || 
-                           count[result] == count[str] && result.CompareTo(str) > 0)
-                        {
+                        
+                        if(result.Equals("") || count[result] < count[str] || count[result] == count[str] && result.CompareTo(str) > 0) 
                             result = str;
-                        }
                     }
                 }
             }
         }
+        
         var r = result.Split(" ");
-        var finalResult = new List<string>();
-
-        foreach(var str in r)
+        
+        return r.ToList();
+    }
+    
+    private Dictionary<string, List<VisitInfo>> GetVisitInfos(string[] username, int[] timestamp, string[] website)
+    {
+        var map = new Dictionary<string, List<VisitInfo>>();
+        
+        // Collect the website info for every user, key: username, value : timestamp and website
+        for(int i = 0; i < username.Length; i++)
         {
-            finalResult.Add(str);
+            if(!map.ContainsKey(username[i])) map.Add(username[i], new List<VisitInfo>());
+            map[username[i]].Add(new VisitInfo(timestamp[i], website[i]));
         }
-        return finalResult;
+        
+        return map;
     }
 }
 
-public class Pair
+public class VisitInfo
 {
     public int Time { get; set; }
     public string Web { get; set; }
     
-    public Pair(int time, string web)
+    public VisitInfo(int time, string web)
     {
         Time = time;
         Web = web;
