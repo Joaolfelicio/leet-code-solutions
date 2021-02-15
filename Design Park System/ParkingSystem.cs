e99788b30a7df8dc5288682cd146b99073fce70f
Joao <joaolfelicio@gmail.com>
2021-02-15 20:54:12 +0100

    Refactor Design Park System

--- Design Park System/ParkingSystem.cs
+++ Design Park System/ParkingSystem.cs
@@ -1,34 +1,29 @@
 public class ParkingSystem 
 {
-    int numOfBig = 0;
-    int numOfMedium = 0;
-    int numOfSmall = 0;
-    
+    public int BigSpaces {get; set;}
+    public int MediumSpaces {get; set;}
+    public int SmallSpaces {get; set;}
+
     public ParkingSystem(int big, int medium, int small) 
     {
-        numOfBig = big;
-        numOfMedium = medium;
-        numOfSmall = small;
+        BigSpaces = big;
+        MediumSpaces = medium;
+        SmallSpaces = small;
     }
     
     public bool AddCar(int carType) 
     {
-        if(carType == 1 && numOfBig > 0)
-        {
-            numOfBig--;
-            return true;
-        }
-        else if(carType == 2 && numOfMedium > 0)
-        {
-            numOfMedium--;
-            return true;
-        }
-        else if(carType == 3 && numOfSmall > 0)
-        {
-            numOfSmall--;
-            return true;
-        }
-        return false;
+        return ReduceSpace(carType);
+    }
+    
+    private bool ReduceSpace(int carType)
+    {
+        if(carType == 1 && BigSpaces > 0) BigSpaces--;
+        else if(carType == 2 && MediumSpaces > 0) MediumSpaces--;
+        else if(carType == 3 && SmallSpaces > 0) SmallSpaces--;
+        else return false;
+        
+        return true;
     }
 }
 
