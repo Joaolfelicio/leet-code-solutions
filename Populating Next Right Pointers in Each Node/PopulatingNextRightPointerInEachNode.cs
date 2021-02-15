9e73602023ece300026bcfbcdfcaf8532c01b46f
Joao <joaolfelicio@gmail.com>
2021-02-15 18:43:25 +0100

    Refactor Populating Next Right Pointers in Each Node

--- Populating Next Right Pointers in Each Node/PopulatingNextRightPointerInEachNode.cs
+++ Populating Next Right Pointers in Each Node/PopulatingNextRightPointerInEachNode.cs
@@ -1,7 +1,5 @@
-using System;
-
 /*
-Definition for a Node.
+// Definition for a Node.
 public class Node {
     public int val;
     public Node left;
@@ -23,61 +21,18 @@ public class Node {
 }
 */
 
-// BFS Approach
 public class Solution 
 {
     public Node Connect(Node root) 
     {
         if(root == null) return null;
         
-        var queue = new Queue<Node>();
-        queue.Enqueue(root);
+        if(root.left != null) root.left.next = root.right;
+        if(root.right != null && root.next != null) root.right.next = root.next.left;
         
-        while(queue.Count > 0)
-        {
-            int depthLength = queue.Count;
-                        
-            for(int i = 0; i < depthLength; i++)
-            {
-                Node current = queue.Dequeue();
-                
-                //If it's not the last node of a level
-                if(i != depthLength - 1)
-                {
-                    current.next = queue.Peek();
-                }
-                
-                if(current.left != null) queue.Enqueue(current.left);
-                if(current.right != null) queue.Enqueue(current.right);
-            }
-        }        
-        return root;
-    }   
-}
-
-// Recursive way
-public class Solution2 
-{
-    public Node Connect(Node root) 
-    {
-        connect(root);
-        return root;
-    }   
-    
-    private void connect(Node root) {
+        Connect(root.left);
+        Connect(root.right);
         
-        if(root == null)
-            return;
-
-        if(root.left != null){
-
-            root.left.next = root.right;
-
-            if(root.next != null)
-                root.right.next = root.next.left;
-        }
-
-        connect(root.left);
-        connect(root.right);
+        return root;
     }
 }
