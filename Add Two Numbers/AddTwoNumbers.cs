a972bb23171b5db2e5ceec9acebd0317f2e71607
Joao <joaolfelicio@gmail.com>
2021-02-23 19:08:43 +0100

    Refactor AddTwoNumbers

--- Add Two Numbers/AddTwoNumbers.cs
+++ Add Two Numbers/AddTwoNumbers.cs
@@ -9,43 +9,36 @@
  *     }
  * }
  */
-public class Solution 
-{
+public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2) 
     {
+        if(l1 == null && l2 == null) return null;
         if(l1 == null) return l2;
         if(l2 == null) return l1;
         
-        var rest = 0;
+        var currL1 = l1;
+        var currL2 = l2;
         var dummyHead = new ListNode();
-        var current = dummyHead;
+        var curr = dummyHead;
+        var rest = 0;
         
-        while(l1 != null || l2 != null)
+        while(currL1 != null || currL2 != null)
         {
-            var sum = 0;
+            var l1val = currL1 != null ? currL1.val : 0;
+            var l2val = currL2 != null ? currL2.val : 0;
             
-            if(l1 != null)
-            {
-                sum += l1.val;
-                l1 = l1.next;
-            }
+            var sum = l1val + l2val + rest;
             
-            if(l2 != null) 
-            {
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            
-            sum += rest;
+            curr.next = new ListNode(sum % 10);
             
             rest = sum / 10;
             
-            current.next = new ListNode(sum % 10);
-            
-            current = current.next;
+            curr = curr.next;
+            if(currL1 != null) currL1 = currL1.next;
+            if(currL2 != null) currL2 = currL2.next;
         }
         
-        if(rest > 0) current.next = new ListNode(1);
+        if(rest == 1) curr.next = new ListNode(rest);
         
         return dummyHead.next;
     }
